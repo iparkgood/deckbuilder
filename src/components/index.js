ae0220778c8aae7010dc879b12f51302e2a87912
iparkgood <zuzu8723@gmail.com>
2021-05-22 11:58:18 -0400

    commit deckbuilder

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as SearchBar } from "./SearchBar";
+export { default as SearchResults } from "./SearchResults";
+export { default as DeckList } from "./DeckList";
+//use this index.js as a way to export all the other components.
